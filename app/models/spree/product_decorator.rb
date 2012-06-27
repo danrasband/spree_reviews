d07d606a1d4cc2882ea0c0c032155c1681b35f9e
Dan Rasband <dan@agencyfusion.com>
2012-06-27 17:14:55 -0600

    Fix two bugs
    Fixes two bugs: 1) a problem with the product decorator method
    `get_stars` where the avg_rating is apparently a String and 2) using
    SpreeReviews::Config instead of Spree::Config in the review settings
    edit.html.erb view.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,13 +2,13 @@ Spree::Product.class_eval do
   has_many :reviews
 
   def get_stars
-    {:star => ((2.0 * self.avg_rating).round / 2.0), :rcount => reviews_count}
+    {:star => ((2.0 * self.avg_rating.to_f).round / 2.0), :rcount => reviews_count}
   end
 
   def recalculate_rating
     count = reviews_count(true)
     self.avg_rating = (reviews_count > 0) ? (self.reviews.approved.sum(:rating).to_f / count) : 0
-    self.update_attributes(:avg_rating => avg_rating, :reviews_count => count)
+    self.update_attributes(:avg_rating => avg_rating.to_f, :reviews_count => count)
   end
 
   private
